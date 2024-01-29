cbf1bf2fa92be95b9151019ac213cc66f8ddfa9e
KingDerick <derick.de-silva@student.nbi-handelsakademin.se>
2024-01-29 12:49:59 +0100

    file structure done, initial game plan

---
+++ memory.js
@@ -0,0 +1,24 @@
+var errors= 0;
+var cardList = [
+    "blue-car",
+    "orange-car",
+    "white-car",
+    "black-car",
+    "yellow-car",
+    "pink-car",
+    "green-car",
+    "red-car",
+
+]
+
+var cardset;
+var board = [];
+
+window.onload = function() {
+  shuffleCards();
+  startGame();
+}
+
+function shuffleCards(){
+  
+}
